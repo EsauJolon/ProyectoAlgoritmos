a4b56270cb914dd1b7c7ab63ccf511f974e29c71
esau-jolon <ejolon@controlsystemsgt.com>
2024-08-07 20:50:10 -0600

    commit inicial

---
+++ Descuento/Descuento.java
@@ -0,0 +1,59 @@
+/*
+ * Se importa Locale para que el scanner use el punto como separador decimal.
+ * Se importa Scanner para poder leer los datos que ingresa el usuario.
+ */
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * La clase Descuento implementa la logica que
+ * manipula el precio del articulo haciendole el descuento para luego sumarle el IVA.
+ * @link Descuento
+ * @author Emanuel Jolón
+ * @version 1.0
+ * @since 02/08/2024
+ * 
+ */
+
+
+public class Descuento{
+
+    /**
+     * El metodo devuelve el total a pagar por el articulo y el descuento con
+     * los datos proporcionados por el usuario 
+     * 
+     * @param precio valor total del producto a comprar.
+     * @return devuelve el total de dolares a adquirir.
+     */    
+
+    public static void main(String[] args) {
+     
+        /*
+         * Se declara la variable scan para poder obtener el valor de
+         * del producto a comprar.
+         * 
+         * se usa tipo double en los datos para poder usar datos con decimales.
+         */
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+		System.out.println("Ingrese el precio del producto:");
+		double precio = scan.nextDouble();
+
+        /*
+         * El descuento se calcula multiplicando el precio por 0.20
+        */
+        double descuento = precio * 0.20;
+        /*
+         * El descuento con descuento se calcula restando el precio - descuento.
+        */
+        double precioD = precio - descuento;
+        /*
+         * El precio final se calcula multiplicando por 1.12 que es el IVA.
+         */
+        double precioF = precioD * 1.12;
+        
+		System.out.println("El precio a pagar por el articulo es de: "+ precioF);
+        System.out.println("Su descuento es de: "+ descuento);	
+
+        
+    }
+}
---
+++ Descuento/documentacion/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Descuento","l":"Descuento()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Descuento","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
